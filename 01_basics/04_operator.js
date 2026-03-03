efae05c7815ae050269ea06c351658e540ebb2db
JAY SONI <jaysoni9549@gmail.com>
2026-03-03 18:59:58 +0530

    Operators

---
+++ 01_basics/04_operator.js
@@ -0,0 +1,58 @@
+console.log("Operator in Js")
+let a = 10, b = 3;
+
+// 1. Arithmatic Operator
+
+console.log("a + b = ",a+b)
+console.log("a - b = ",a-b)
+console.log("a * b = ",a*b)
+console.log("a / b = ",a/b)
+console.log("a % b = ",a%b)
+console.log("a ^ b = ",a**b)
+
+// increment and decrement operator
+console.log("a++ = ",a++)
+console.log("a = ",a)
+console.log("a-- = ",a--)
+console.log("a = ",a)
+
+console.log("++b = ",++b)
+console.log("b = ",b)
+console.log("--b = ",--b)
+console.log("b = ",b)
+
+
+// 2. Assignment operator
+
+let c = 1, d=2;
+c = d
+console.log(c)
+c += 5
+console.log(c)
+c -= 6
+console.log(c)
+c *= 4
+console.log(c)
+c /= d
+console.log(c)
+c %= 8
+console.log(c)
+c **= d
+console.log(c)
+
+// 3. Comparison Operator
+
+let comp1 = 6
+let comp2 = "6"
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 !== comp2 is ", comp1 !== comp2) // also check datatype
+console.log("comp1 === comp2 is ", comp1 === comp2) // also check datatype
+
+// 4. Logical Operator
+let x = 5, y = 6
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(x>y || x!=6)
+console.log(!false)
+console.log(!true)
---
+++ 01_basics/Practice_Set/01_practice.js
@@ -0,0 +1,31 @@
+// Question 1:
+let a = "Hrithik"
+let b = 6
+console.log(a+b)
+
+// Question 2:
+console.log(typeof (a))
+console.log(typeof b)
+console.log(typeof (a+b))
+
+//Question 3:
+const Obj = {
+    name: "Hrithik",
+    section: 1,
+    isPrincipal: false
+}
+// Obj = "JAY" , this will show error
+
+// Question 4:
+Obj['friend'] = "Rahul"
+Obj['name'] = "Rakesh"
+console.log(Obj)
+
+// Question 5:
+const Dict = {
+    appreciate: "recognize the full worth of.",
+    atarxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "Work, especially hard work"
+}
+console.log(Dict['yakka'])
+console.log(Dict.yakka)
