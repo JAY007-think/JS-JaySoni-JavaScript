961e4ba65989deef294e1b845196027c7c2d6d46
JAY SONI <jaysoni9549@gmail.com>
2026-03-03 17:37:56 +0530

    datatype conversions

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,14 +1,29 @@
 "use strict"; //treat all JS code as newer version 
 
+// Primittive datatype - 7 types of datatypes: trick => N N B B S S U
+// N - null
+// N - Number
+// B - Boolean
+// B - BigInt
+// S - String
+// S - Symbol
+// U - undefined
+
 // alert( 3 + 3 )  // we are using Node.js not browser
 
 console.log(3+3); console.log("Jayz") // if we use ";" between them, then valid but not readable
 
-let name = "Rahul"
-let age = 18
-let isLoggedIn = false
-let state;
-let status = null
+let status = null;
+let age = 18;
+let isLoggedIn = false;
+let sum = BigInt("56") + BigInt("4");
+let name = "Rahul";
+let sign = Symbol("I am nice symbol");
+let error = undefined;
+let a
+
+console.log(status,age,isLoggedIn,sum,name,sign,error,a);
+console.log(typeof status)
 
 // if number => 2 to power 53 , then use bigint
 // string => "Jay"
@@ -17,7 +32,16 @@ let status = null
 // undefined
 // symbol => unique
 
-// object
+
+// Non primittive datatype - object
+const item = {
+    "Harsh": true,
+    "Rahul": false,
+    "Lovish": 67,
+    "Jay": undefined
+}
+
+console.log(item["Harsh"]) // true
 
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
